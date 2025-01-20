2a574054d2be06d999db579867df8938fffae391
DimaA <azardimka@gmail.com>
2025-01-21 02:25:15 +0300

    #1 создал ветку девелоп. Добавил НуГет пакет, для авторизации. Немного изменил файл программ, добавил туда авторизацию.

--- Sodruzhestvo Finance/SFinance/Program.cs
+++ Sodruzhestvo Finance/SFinance/Program.cs
@@ -1,3 +1,11 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
 namespace SFinance
 {
     public class Program
@@ -6,10 +14,53 @@ namespace SFinance
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddAuthorization();
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        // указывает, будет ли валидироваться издатель при валидации токена
+                        ValidateIssuer = true,
+                        // строка, представляющая издателя
+                        ValidIssuer = AuthOptions.ISSUER,
+                        // будет ли валидироваться потребитель токена
+                        ValidateAudience = true,
+                        // установка потребителя токена
+                        ValidAudience = AuthOptions.AUDIENCE,
+                        // будет ли валидироваться время существования
+                        ValidateLifetime = true,
+                        // установка ключа безопасности
+                        IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+                        // валидация ключа безопасности
+                        ValidateIssuerSigningKey = true,
+                    };
+                });
+            var app = builder.Build();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.Map("/login/{username}", (string username) =>
+            {
+                var claims = new List<Claim> { new Claim(ClaimTypes.Name, username) };
+                // создаем JWT-токен
+                var jwt = new JwtSecurityToken(
+                issuer: AuthOptions.ISSUER,
+                audience: AuthOptions.AUDIENCE,
+                        claims: claims,
+                        expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(2)),
+                        signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+
+                return new JwtSecurityTokenHandler().WriteToken(jwt);
+            });
+
+            app.Map("/data", [Authorize] () => new { message = "Hello World!" });
+
+            app.Run();
+
             // Add services to the container.
             builder.Services.AddControllersWithViews();
-
-            var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
@@ -19,7 +70,7 @@ namespace SFinance
                 app.UseHsts();
             }
 
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
@@ -31,6 +82,15 @@ namespace SFinance
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
+        }
+
+        public class AuthOptions
+        {
+            public const string ISSUER = "MyAuthServer"; // издатель токена
+            public const string AUDIENCE = "MyAuthClient"; // потребитель токена
+            const string KEY = "mysupersecret_secretsecretsecretkey!123";   // ключ для шифрации
+            public static SymmetricSecurityKey GetSymmetricSecurityKey() =>
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
         }
     }
 }
