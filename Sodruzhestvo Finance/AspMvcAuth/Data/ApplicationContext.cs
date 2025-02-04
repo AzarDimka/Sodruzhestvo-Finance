bc5a3cb7022827983361ce1cc9f7c0737e8d7b75
DimaA <azardimka@gmail.com>
2025-02-04 23:40:29 +0300

    #3 создал классы

---
+++ Sodruzhestvo Finance/AspMvcAuth/Data/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using AspMvcAuth.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspMvcAuth.Data
+{
+    public class ApplicationContext : IdentityDbContext<ApplicationUser, ApplicationRole, int>
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Sodruzhestvo Finance/AspMvcAuth/Models/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AspMvcAuth.Models
+{
+    public class ApplicationUser : IdentityUser<int>
+    {
+        /// <summary>
+        /// День рождения
+        /// </summary>
+        public DateOnly Birthday { get; set; }
+
+        /// <summary>
+        /// Организация
+        /// </summary>
+        public string Organization { get; set; }
+
+        /// <summary>
+        /// Департамент
+        /// </summary>
+        public string Department { get; set; }
+    }
+}
